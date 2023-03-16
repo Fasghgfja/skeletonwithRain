b861cffee8d6d3f6205ac7de6c645ccb96c9434f
Giuliano Giambertone csaz5789 <Giuliano.Giambertone@student.uibk.ac.at>
2023-03-16 18:01:27 +0100

    changed user roles and fixed tests and webconfig

--- src/main/java/at/qe/skeleton/model/UserRole.java
+++ src/main/java/at/qe/skeleton/model/UserRole.java
@@ -2,14 +2,13 @@ package at.qe.skeleton.model;
 
 /**
  * Enumeration of available user roles.
- *
  * This class is part of the skeleton project provided for students of the
  * course "Software Engineering" offered by the University of Innsbruck.
  */
 public enum UserRole {
 
     ADMIN,
-    MANAGER,
-    EMPLOYEE
+    USER,
+    GARDENER
 
 }
