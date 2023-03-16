f9978d055d7a043ebdb140be3bfb80e8b236703f
Giuliano Giambertone csaz5789 <Giuliano.Giambertone@student.uibk.ac.at>
2023-03-16 19:22:49 +0100

    changed javadoc in user repository

--- src/main/java/at/qe/skeleton/repositories/UserxRepository.java
+++ src/main/java/at/qe/skeleton/repositories/UserxRepository.java
@@ -8,20 +8,48 @@ import org.springframework.data.repository.query.Param;
 
 /**
  * Repository for managing {@link Userx} entities.
- *
  * This class is part of the skeleton project provided for students of the
  * course "Software Engineering" offered by the University of Innsbruck.
  */
 public interface UserxRepository extends AbstractRepository<Userx, String> {
 
-
+    /**
+     * The method Retrieves a User entity by its Username.
+     *
+     * @param username must not be {@literal null}.
+     * @return The user with the given name.
+     * @throws IllegalArgumentException If username is {@literal null}.
+     */
     Userx findFirstByUsername(String username);
 
+    /**
+     * The method Retrieves a List of User entities by part of its Username.
+     *
+     * @param username string that has to be present inside the username, must not be {@literal null}.
+     * @return A list of users containing the given string in their username.
+     * @throws IllegalArgumentException If username is {@literal null}.
+     */
     List<Userx> findByUsernameContaining(String username);
 
+    /**
+     * The method Retrieves a List of User entities searching by a string containing both firstname and lastname separated
+     * by a space.
+     *
+     * @param wholeName String composed by The First and second name of the user we want to search for separated by space
+     *                  ,must not be {@literal null}.
+     * @return A list of users corresponding to the searched wholeName.
+     * @throws IllegalArgumentException If wholeName is {@literal null}.
+     */
     @Query("SELECT u FROM Userx u WHERE CONCAT(u.firstName, ' ', u.lastName) = :wholeName")
     List<Userx> findByWholeNameConcat(@Param("wholeName") String wholeName);
 
+    /**
+     * The method Retrieves a List of User entities searching by role {@link UserRole}.
+     *
+     * @param role UserRole to be searched for ,must not be {@literal null}.
+     * @return a list of all users with the selected role.
+     * @throws IllegalArgumentException If role is {@literal null}.
+     */
     @Query("SELECT u FROM Userx u WHERE :role MEMBER OF u.roles")
     List<Userx> findByRole(@Param("role") UserRole role);
 
