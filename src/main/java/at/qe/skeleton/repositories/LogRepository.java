ca3e24bbcbe7c7e506c0c781601d619fbc686dbe
Giuliano Giambertone csaz5789 <Giuliano.Giambertone@student.uibk.ac.at>
2023-03-16 19:27:28 +0100

    log repository

---
+++ src/main/java/at/qe/skeleton/repositories/LogRepository.java
@@ -0,0 +1,9 @@
+package at.qe.skeleton.repositories;
+import at.qe.skeleton.model.Log;
+
+
+/**
+ * Repository for managing the {@link Log} Log entities.
+ */
+public interface LogRepository extends AbstractRepository<Log, Long> {
+}
