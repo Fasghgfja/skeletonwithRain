6d007b625f5950d9dc1df648449e97373fe7d260
Giuliano Giambertone csaz5789 <Giuliano.Giambertone@student.uibk.ac.at>
2023-03-17 11:15:04 +0100

    Measurement model update

--- src/main/java/at/qe/skeleton/model/Measurement.java
+++ src/main/java/at/qe/skeleton/model/Measurement.java
@@ -1,7 +1,59 @@
 package at.qe.skeleton.model;
 
-public class Measurement {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
-    MeasurementType type;
-    Double value;
+@Entity
+public class Measurement {
+    @Id
+    private Long id;
+    @Column
+    private Long plantID;
+    @Column
+    private Double value;
+    @Column
+    private String unit;
+    @Column
+    private MeasurementType type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPlantID() {
+        return plantID;
+    }
+
+    public void setPlantID(Long plantID) {
+        this.plantID = plantID;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public MeasurementType getType() {
+        return type;
+    }
+
+    public void setType(MeasurementType type) {
+        this.type = type;
+    }
 }
