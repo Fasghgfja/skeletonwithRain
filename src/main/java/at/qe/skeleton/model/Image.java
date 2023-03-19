255e00bdf6f73ec70d1d8aaa65a6fc244574e80f
Giuliano Giambertone csaz5789 <Giuliano.Giambertone@student.uibk.ac.at>
2023-03-19 15:40:50 +0100

    first image model

---
+++ src/main/java/at/qe/skeleton/model/Image.java
@@ -0,0 +1,73 @@
+package at.qe.skeleton.model;
+
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+
+
+/**
+ * JPA Entity to Represent an Image.
+ */
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue
+    @Column(nullable = false, unique = true)
+    private Long imageId;
+
+
+    @ManyToOne
+    @JoinColumn(name = "plant_id", nullable = false)
+    private Plant plant;
+
+    @Column
+    private String imageName;
+
+    @Column
+    private String author;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate date;
+
+
+
+    @Column(length = 50000000)
+    private byte[] imageByte;
+
+    public Image() {
+
+    }
+
+    public Image(String imageName, byte[] picByte) {
+        this.imageName = imageName;
+    }
+
+    public Long getId() {return imageId;}
+
+    public void setId(Long imageId) {this.imageId = imageId;}
+
+    public byte[] getImageByte() {
+        return imageByte;
+    }
+
+    public void setImageByte(byte[] imageByte) {
+        this.imageByte = imageByte;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
