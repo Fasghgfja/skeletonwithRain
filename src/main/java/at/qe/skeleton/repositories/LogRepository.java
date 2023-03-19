1cfd0924f1d65d042274a83509435c2e57f8a407
Giuliano Giambertone csaz5789 <Giuliano.Giambertone@student.uibk.ac.at>
2023-03-19 15:48:29 +0100

    javadoc to LogRepository and User Repository updated

--- src/main/java/at/qe/skeleton/repositories/LogRepository.java
+++ src/main/java/at/qe/skeleton/repositories/LogRepository.java
@@ -4,6 +4,9 @@ import at.qe.skeleton.model.Log;
 
 /**
  * Repository for managing the {@link Log} Log entities.
+ *  * The Derived Query are split into two parts separated by keywords:
+ *  * The first one is the introducer(e.g find.., read.., query.., ...)
+ *  * The second one defines the criteria (e.g ...ByName, ...).
  */
 public interface LogRepository extends AbstractRepository<Log, Long> {
 }
