470fccea5b6c466d9ce6f3e1125be86941288aa8
Deniz Özkaya <deniz.oezkaya@student.uibk.ac.at>
2023-03-20 13:52:39 +0100

    update api

--- src/main/java/at/qe/skeleton/api/services/MeasurementService.java
+++ src/main/java/at/qe/skeleton/api/services/MeasurementService.java
@@ -1,11 +1,12 @@
 package at.qe.skeleton.api.services;
 
-import at.qe.skeleton.api.exceptions.MeasurementNotFoundException;
-import at.qe.skeleton.api.model.Measurement;
-import org.springframework.stereotype.Service;
-
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+import at.qe.skeleton.api.exceptions.MeasurementNotFoundException;
+import at.qe.skeleton.api.model.Measurement;
 
 @Service
 public class MeasurementService {
@@ -13,7 +14,7 @@ public class MeasurementService {
     private static final AtomicLong ID_COUNTER = new AtomicLong(1);
     private static final ConcurrentHashMap<Long, Measurement> measurements = new ConcurrentHashMap<>();
 
-    public Measurement addMeasurement(Measurement measurement){
+    public Measurement addMeasurement(Measurement measurement) {
         Measurement newMeasurement = new Measurement();
         newMeasurement.setId(ID_COUNTER.getAndIncrement());
         newMeasurement.setPlantID(measurement.getPlantID());
@@ -28,11 +29,42 @@ public class MeasurementService {
 
     public Measurement findOneMeasurement(Long id) throws MeasurementNotFoundException {
         Measurement measurement = measurements.get(id);
-        if(measurement != null){
+        if (measurement != null)
             return measurement;
-        }
-        else{
+        else
             throw new MeasurementNotFoundException();
-        }
     }
+
+    public Measurement updateMeasurement(long id, Measurement measurement) {
+        if (measurement.getPlantID() != null)
+
+            measurements.computeIfPresent(id, (key, value) -> {
+                value.setPlantID(measurement.getPlantID());
+                return value;
+            });
+
+        if (measurement.getValue() != null)
+
+            measurements.computeIfPresent(id, (key, value) -> {
+                value.setValue(measurement.getValue());
+                return value;
+            });
+
+        if (measurement.getUnit() != null)
+            measurements.computeIfPresent(id, (key, value) -> {
+                value.setUnit(measurement.getUnit());
+                return value;
+            });
+
+        if (measurement.getType() != null)
+            measurements.computeIfPresent(id, (key, value) -> {
+                value.setType(measurement.getType());
+                return value;
+            });
+
+        return measurements.get(id);
+
+        //there are more elegant ways to do this: https://www.baeldung.com/spring-rest-json-patch
+    }
+
 }
