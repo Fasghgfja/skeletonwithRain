5025dc945df009ddd711262c22762f5085575ae3
Marco Ponti csaw9903 <marco.ponti@student.uibk.ac.at>
2023-03-25 15:20:50 +0100

    update metadata model

--- src/main/java/at/qe/skeleton/model/Metadata.java
+++ src/main/java/at/qe/skeleton/model/Metadata.java
@@ -1,12 +1,12 @@
 package at.qe.skeleton.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
 
 /**
- * Abstract Metadata to save and retrieve creatinDate and updateDate of entities.
+ * Abstract Metadata to save and retrieve createDate and updateDate of entities.
  */
 @MappedSuperclass
 public abstract class Metadata {
@@ -15,6 +15,30 @@ public abstract class Metadata {
     private LocalDate createDate;
     @Column(columnDefinition = "DATE")
     private LocalDate updateDate;
+
+    @ManyToOne
+    @JoinColumn(name="crateUser")
+    private Userx createUser;
+
+    @ManyToOne
+    @JoinColumn(name="updateUser")
+    private Userx updateUser;
+
+    public Userx getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(Userx createUser) {
+        this.createUser = createUser;
+    }
+
+    public Userx getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(Userx updateUser) {
+        this.updateUser = updateUser;
+    }
 
     public LocalDate getCreateDate() {
         return createDate;
