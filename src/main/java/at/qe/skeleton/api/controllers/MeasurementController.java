641add200fd34ce3a6500af3ff69a13a348cfd59
Marco Ponti csaw9903 <marco.ponti@student.uibk.ac.at>
2023-03-27 17:58:06 +0200

    update measurement controller

--- src/main/java/at/qe/skeleton/api/controllers/MeasurementController.java
+++ src/main/java/at/qe/skeleton/api/controllers/MeasurementController.java
@@ -1,5 +1,6 @@
 package at.qe.skeleton.api.controllers;
 
+import at.qe.skeleton.model.Measurement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,40 +10,43 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
 import at.qe.skeleton.api.exceptions.MeasurementNotFoundException;
-import at.qe.skeleton.api.model.Measurement;
 import at.qe.skeleton.api.services.MeasurementService;
 
 @RestController
 public class MeasurementController {
+    /**
 
-    @Autowired
-    MeasurementService measurementService;
 
-    @PostMapping("/api/measurements")
-    Measurement createMeasurement(@RequestBody Measurement measurement)
-    {
-        return measurementService.addMeasurement(measurement);
-    }
 
-    @GetMapping("/api/measurements/{id}")
-    Measurement getOneMeasurement(@PathVariable Long id)
-    {
-        try
-        {
-            return measurementService.findOneMeasurement(id);
-        }
-        catch(MeasurementNotFoundException ex)
-        {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
-    }
+     @Autowired
+     MeasurementService measurementService;
 
-    @PatchMapping("/api/measurements/{id}")
-    Measurement updateMeasurement(@PathVariable long id, @RequestBody Measurement measurement)
-    {
-        return measurementService.updateMeasurement(id, measurement);
-    }
+     @PostMapping("/api/measurements")
+     Measurement createMeasurement(@RequestBody Measurement measurement)
+     {
+     return measurementService.addMeasurement(measurement);
+     }
+
+     @GetMapping("/api/measurements/{id}")
+     Measurement getOneMeasurement(@PathVariable Long id)
+     {
+     try
+     {
+     return measurementService.findOneMeasurement(id);
+     }
+     catch(MeasurementNotFoundException ex)
+     {
+     throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+     }
+     }
+
+     @PatchMapping("/api/measurements/{id}")
+     Measurement updateMeasurement(@PathVariable long id, @RequestBody Measurement measurement)
+     {
+     return measurementService.updateMeasurement(id, measurement);
+     }
+     */
+
 
 }
