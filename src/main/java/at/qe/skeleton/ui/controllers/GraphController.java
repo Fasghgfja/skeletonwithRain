3264dd277d83e3ece71097c99d23a86206fc2a5d
Giambertone Giuliano csaz5789 <Giuliano.Giambertone@student.uibk.ac.at>
2023-03-27 18:08:53 +0200

    graph controller for dashboard

--- src/main/java/at/qe/skeleton/ui/controllers/GraphController.java
+++ src/main/java/at/qe/skeleton/ui/controllers/GraphController.java
@@ -1,12 +1,13 @@
 package at.qe.skeleton.ui.controllers;
 
 
-import jakarta.annotation.PostConstruct;
-import jakarta.faces.application.FacesMessage;
-import jakarta.faces.context.FacesContext;
+import at.qe.skeleton.api.services.MeasurementService;
+import at.qe.skeleton.model.Measurement;
+import at.qe.skeleton.model.SensorStation;
+import at.qe.skeleton.services.SensorStationService;
 import lombok.Getter;
 import lombok.Setter;
-import org.primefaces.event.ItemSelectEvent;
+import org.primefaces.event.SelectEvent;
 import org.primefaces.model.charts.ChartData;
 import org.primefaces.model.charts.axes.cartesian.CartesianScales;
 import org.primefaces.model.charts.axes.cartesian.linear.CartesianLinearAxes;
@@ -21,6 +22,7 @@ import org.primefaces.model.charts.optionconfig.animation.Animation;
 import org.primefaces.model.charts.optionconfig.legend.Legend;
 import org.primefaces.model.charts.optionconfig.legend.LegendLabel;
 import org.primefaces.model.charts.optionconfig.title.Title;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import java.io.Serializable;
@@ -33,17 +35,139 @@ import java.util.List;
 @Scope("view")
 public class GraphController implements Serializable {
 
+    /**
+     * Different charts that can be used in the application
+     * to call them from the frontend just use graphcontroller.fieldname
+     * */
     private LineChartModel lineModel;
     private LineChartModel cartesianLinerModel;
-    private BarChartModel barModel;
+    private BarChartModel barModel = new BarChartModel();
+
+    @Autowired
+    MeasurementService measurementService;
 
 
-    @PostConstruct
-    public void init() {
-        createLineModel();
-        createCartesianLinerModel();
-        createBarModel();
+    @Autowired
+    SensorStationService sensorService;
+
+
+    /**
+     * Attribute to cache the currently displayed sensor station.
+     */
+    private SensorStation sensorStation;
+
+    /**
+     * Attribute to cache the latest Measurements.
+     */
+    private List<Measurement> latestMeasurements;
+
+
+
+    /**
+     * Method used in the dashboard to change the Graph displayed based on row selection of the panel beside.
+     */
+    public void onRowSelect(SelectEvent<SensorStation> event) {
+        sensorStation = (SensorStation) event.getObject();
+            createLineModel();
+            createCartesianLinerModel();
+            latestMeasurements = new ArrayList<>(measurementService.getLatestPlantMeasurements(sensorStation));
+        if(!latestMeasurements.isEmpty()) {
+        createBarModel(latestMeasurements);
+        }
     }
+
+    //TODO: hide y axis values for dashboard graph
+    public void createBarModel(List<Measurement> measurements) {
+        barModel = new BarChartModel();
+        ChartData data = new ChartData();
+
+        BarChartDataSet barDataSet = new BarChartDataSet();
+        barDataSet.setLabel("Selected Sensor Station Last Measurements");
+
+        //TODO:change this with a query for AirValue GroundValue HumidityValue etc instead of hoping they come out in the correct order
+        List<Number> values = new ArrayList<>();
+        measurements.forEach(measurement -> {
+            if(measurement == null) {values.add(0);} else {values.add(Integer.parseInt(measurement.getValue_s()));}
+        });
+
+        barDataSet.setData(values);
+        List<String> bgColor = new ArrayList<>();
+        bgColor.add("rgba(255, 99, 132, 0.2)");
+        bgColor.add("rgba(255, 159, 64, 0.2)");
+        bgColor.add("rgba(255, 205, 86, 0.2)");
+        bgColor.add("rgba(75, 192, 192, 0.2)");
+        bgColor.add("rgba(54, 162, 235, 0.2)");
+        bgColor.add("rgba(153, 102, 255, 0.2)");
+        bgColor.add("rgba(201, 203, 207, 0.2)");
+        barDataSet.setBackgroundColor(bgColor);
+
+        List<String> borderColor = new ArrayList<>();
+        borderColor.add("rgb(255, 99, 132)");
+        borderColor.add("rgb(255, 159, 64)");
+        borderColor.add("rgb(255, 205, 86)");
+        borderColor.add("rgb(75, 192, 192)");
+        borderColor.add("rgb(54, 162, 235)");
+        borderColor.add("rgb(153, 102, 255)");
+        borderColor.add("rgb(201, 203, 207)");
+        barDataSet.setBorderColor(borderColor);
+        barDataSet.setBorderWidth(1);
+
+        data.addChartDataSet(barDataSet);
+
+        List<String> labels = new ArrayList<>();
+        labels.add("Temperature");
+        labels.add("Air Humidity");
+        labels.add("Ground Humidity");
+        labels.add("Light Intensity");
+        labels.add("Air Quality");
+        labels.add("Air Pressure");
+        data.setLabels(labels);
+        barModel.setData(data);
+
+        //Options
+        BarChartOptions options = new BarChartOptions();
+        CartesianScales cScales = new CartesianScales();
+        CartesianLinearAxes linearAxes = new CartesianLinearAxes();
+        linearAxes.setOffset(true);
+        linearAxes.setBeginAtZero(true);
+        CartesianLinearTicks ticks = new CartesianLinearTicks();
+        linearAxes.setTicks(ticks);
+        cScales.addYAxesData(linearAxes);
+        options.setScales(cScales);
+
+        Title title = new Title();
+        title.setDisplay(true);
+        title.setText("Bar Chart");
+        options.setTitle(title);
+
+
+        Legend legend = new Legend();
+        legend.setDisplay(true);
+        legend.setPosition("top");
+        LegendLabel legendLabels = new LegendLabel();
+        legendLabels.setFontStyle("italic");
+        legendLabels.setFontColor("#2980B9");
+        legendLabels.setFontSize(24);
+        legend.setLabels(legendLabels);
+        options.setLegend(legend);
+
+
+        // disable animation
+        Animation animation = new Animation();
+        animation.setDuration(0);
+        options.setAnimation(animation);
+
+        barModel.setOptions(options);
+    }
+
+
+
+
+
+
+
+
+
 
 
     public void createLineModel() {
@@ -88,7 +212,6 @@ public class GraphController implements Serializable {
         lineModel.setOptions(options);
         lineModel.setData(Air_Temperature);
     }
-
 
     public void createCartesianLinerModel() {
         cartesianLinerModel = new LineChartModel();
@@ -157,98 +280,5 @@ public class GraphController implements Serializable {
         cartesianLinerModel.setOptions(options);
     }
 
-
-    public void createBarModel() {
-        barModel = new BarChartModel();
-        ChartData data = new ChartData();
-
-        BarChartDataSet barDataSet = new BarChartDataSet();
-        barDataSet.setLabel("My First Dataset");
-
-        List<Number> values = new ArrayList<>();
-        values.add(65);
-        values.add(59);
-        values.add(80);
-        values.add(81);
-        values.add(56);
-        values.add(55);
-        values.add(40);
-        barDataSet.setData(values);
-
-        List<String> bgColor = new ArrayList<>();
-        bgColor.add("rgba(255, 99, 132, 0.2)");
-        bgColor.add("rgba(255, 159, 64, 0.2)");
-        bgColor.add("rgba(255, 205, 86, 0.2)");
-        bgColor.add("rgba(75, 192, 192, 0.2)");
-        bgColor.add("rgba(54, 162, 235, 0.2)");
-        bgColor.add("rgba(153, 102, 255, 0.2)");
-        bgColor.add("rgba(201, 203, 207, 0.2)");
-        barDataSet.setBackgroundColor(bgColor);
-
-        List<String> borderColor = new ArrayList<>();
-        borderColor.add("rgb(255, 99, 132)");
-        borderColor.add("rgb(255, 159, 64)");
-        borderColor.add("rgb(255, 205, 86)");
-        borderColor.add("rgb(75, 192, 192)");
-        borderColor.add("rgb(54, 162, 235)");
-        borderColor.add("rgb(153, 102, 255)");
-        borderColor.add("rgb(201, 203, 207)");
-        barDataSet.setBorderColor(borderColor);
-        barDataSet.setBorderWidth(1);
-
-        data.addChartDataSet(barDataSet);
-
-        List<String> labels = new ArrayList<>();
-        labels.add("Temperature");
-        labels.add("Air Humidity");
-        labels.add("Ground Humidity");
-        labels.add("Light Intensity");
-        labels.add("Air Quality");
-        labels.add("Air Pressure");
-        data.setLabels(labels);
-        barModel.setData(data);
-
-        //Options
-        BarChartOptions options = new BarChartOptions();
-        CartesianScales cScales = new CartesianScales();
-        CartesianLinearAxes linearAxes = new CartesianLinearAxes();
-        linearAxes.setOffset(true);
-        linearAxes.setBeginAtZero(true);
-        CartesianLinearTicks ticks = new CartesianLinearTicks();
-        linearAxes.setTicks(ticks);
-        cScales.addYAxesData(linearAxes);
-        options.setScales(cScales);
-
-        Title title = new Title();
-        title.setDisplay(true);
-        title.setText("Bar Chart");
-        options.setTitle(title);
-
-        Legend legend = new Legend();
-        legend.setDisplay(true);
-        legend.setPosition("top");
-        LegendLabel legendLabels = new LegendLabel();
-        legendLabels.setFontStyle("italic");
-        legendLabels.setFontColor("#2980B9");
-        legendLabels.setFontSize(24);
-        legend.setLabels(legendLabels);
-        options.setLegend(legend);
-
-        // disable animation
-        Animation animation = new Animation();
-        animation.setDuration(0);
-        options.setAnimation(animation);
-
-        barModel.setOptions(options);
-    }
-
-
-
-    public void itemSelect(ItemSelectEvent event) {
-        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Item selected",
-                "Item Index: " + event.getItemIndex() + ", DataSet Index:" + event.getDataSetIndex());
-
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
 
 }
