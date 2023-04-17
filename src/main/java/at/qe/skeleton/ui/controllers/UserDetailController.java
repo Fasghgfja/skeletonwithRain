5929d95f34b753a1eb7e9fe81c20c31e1bb95b1a
Deniz Özkaya csaw1345 <deniz.oezkaya@student.uibk.ac.at>
2023-04-17 23:06:53 +0200

    implement usermanagement

--- src/main/java/at/qe/skeleton/ui/controllers/UserDetailController.java
+++ src/main/java/at/qe/skeleton/ui/controllers/UserDetailController.java
@@ -1,53 +1,59 @@
 package at.qe.skeleton.ui.controllers;
 
+import at.qe.skeleton.model.UserRole;
 import at.qe.skeleton.model.Userx;
 import at.qe.skeleton.services.UserService;
-import java.io.Serializable;
-
-
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-
-import at.qe.skeleton.model.UserRole;
-import org.springframework.stereotype.Controller;
-
-import java.util.HashSet;
-import java.util.Set;
+import java.io.Serializable;
+import java.util.*;
 
 /**
  * Controller for the user detail view.
- *
+ * <p>
  * This class is part of the skeleton project provided for students of the
- * course "Software Engineering" offered by the University of Innsbruck.
+ * course "Software Architecture" offered by Innsbruck University.
  */
-
-@Getter
-@Setter
-@Controller
+@Component
+@Scope("view")
 public class UserDetailController implements Serializable {
 
     @Autowired
-    private UserService userService;
+    private transient UserService userService;
 
+    private List<String> selectedRoles;
+
+    private List<String> selectedRolesEdit;
     /**
      * Attribute to cache the currently displayed user
      */
     private Userx user;
-    private Userx userToBeCreated = new Userx();
-    private String username;
-    private String password;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String phone;
-    private Set<UserRole> roles;
-    private boolean admin;
-    private boolean gardener;
+
+    /**
+     * Returns the currently displayed user.
+     */
+    public Userx getUser() {
+        return user;
+    }
+
+    /**
+     * Sets the currently displayed user and reloads it form db. This user is
+     * targeted by any further calls of
+     * {@link #doReloadUser()}, {@link #doSaveUser()} and
+     * {@link #doDeleteUser()}.
+     */
+    public void setUser(Userx user) {
+        this.user = user;
+        this.selectedRolesEdit = new ArrayList<>();
+        Set<UserRole> newRoles  = this.user.getRoles();
+        for (UserRole userRole: newRoles) {
+            this.selectedRolesEdit.add(userRole.name());
+        }
+
+        doReloadUser();
+    }
 
     /**
      * Action to force a reload of the currently displayed user.
@@ -60,8 +66,25 @@ public class UserDetailController implements Serializable {
      * Action to save the currently displayed user.
      */
     public void doSaveUser() {
+        Set<UserRole> roles = new HashSet<>();
+        if (selectedRolesEdit != null) {
+            for (String role : selectedRolesEdit) {
+                if (role.equals("ADMIN")) {
+                    roles.add(UserRole.ADMIN);
+                    roles.add(UserRole.GARDENER);
+                    roles.add(UserRole.USER);
+                } else if (role.equals("GARDENER")) {
+                    roles.add(UserRole.GARDENER);
+                    roles.add(UserRole.USER);
+                } else{
+                    roles.add(UserRole.USER);
+                }
+            }
+        }
+        user.setRoles(roles);
         user = this.userService.saveUser(user);
     }
+
 
     /**
      * Action to delete the currently displayed user.
@@ -71,18 +94,12 @@ public class UserDetailController implements Serializable {
         user = null;
     }
 
-    public void doCreateUser() {
-        roles = new HashSet<>();
-        roles.add(UserRole.USER);
-        if(admin){
-            roles.add(UserRole.GARDENER);
-            roles.add(UserRole.ADMIN);
-        } else if(gardener){
-            roles.add(UserRole.GARDENER);
-        }
-        this.userService.createUser(username,password, firstName,lastName,email, phone, roles);
-        this.userToBeCreated = null;
+    public List<String> getSelectedRolesEdit() {
+        return selectedRolesEdit;
     }
 
+    public void setSelectedRolesEdit(List<String> selectedRolesEdit) {
+        this.selectedRolesEdit = selectedRolesEdit;
+    }
 
 }
