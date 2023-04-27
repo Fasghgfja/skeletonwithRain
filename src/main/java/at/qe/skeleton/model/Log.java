3da7f0940346cb654d3fcf8e496d7189fdf14f56
Deniz Özkaya - csaw1345 <deniz.oezkaya@student.uibk.ac.at>
2023-04-27 17:51:04 +0200

    LogService tests

--- src/main/java/at/qe/skeleton/model/Log.java
+++ src/main/java/at/qe/skeleton/model/Log.java
@@ -10,7 +10,7 @@ import org.springframework.data.domain.Persistable;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
+import java.util.Objects;
 
 
 //TODO: Fix like the example in the demo
@@ -42,6 +42,26 @@ public class Log implements Persistable<Long>, Serializable, Comparable<Log> {
 
     @Enumerated(EnumType.STRING)
     private LogType type;
+
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof final Log other)) {
+            return false;
+        }
+        return Objects.equals(this.id, other.id);
+    }
+
 
     @Override
     public int compareTo(Log o) {
