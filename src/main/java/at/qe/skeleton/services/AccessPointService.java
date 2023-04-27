7c02da6b731a91c579b85c2b34611f5a6f9e2b6a
Deniz Özkaya - csaw1345 <deniz.oezkaya@student.uibk.ac.at>
2023-04-27 23:15:03 +0200

    AccessPointService tests

--- src/main/java/at/qe/skeleton/services/AccessPointService.java
+++ src/main/java/at/qe/skeleton/services/AccessPointService.java
@@ -2,21 +2,13 @@ package at.qe.skeleton.services;
 
 import at.qe.skeleton.model.*;
 import at.qe.skeleton.repositories.AccessPointRepository;
-import at.qe.skeleton.repositories.LogRepository;
-import at.qe.skeleton.repositories.SensorStationRepository;
-import at.qe.skeleton.repositories.UserxRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import javax.sql.RowSet;
+
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.Collection;
 
 @Service
@@ -25,14 +17,14 @@ public class AccessPointService {
 
     @Autowired
     private AccessPointRepository accessPointRepository;
-    @Autowired
-    private LogRepository logRepository;
-    @Autowired
-    private UserxRepository userRepository;
 
     @PreAuthorize("hasAuthority('ADMIN')")
     public Collection<AccessPoint> getAllAccessPoint() {
         return accessPointRepository.findAll();
+    }
+
+    public AccessPoint getFirstById(Long id){
+        return accessPointRepository.findFirstById(id);
     }
 
 
@@ -59,49 +51,6 @@ public class AccessPointService {
      */
     @PreAuthorize("hasAuthority('ADMIN')")
     public void deleteAccessPoint(AccessPoint accessPoint) {
-        Log deleteLog = new Log();
-        deleteLog.setDate(LocalDate.now());
-        deleteLog.setTime(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
-        deleteLog.setAuthor(getAuthenticatedUser().getUsername());
-        deleteLog.setSubject("AP DELETION");
-        deleteLog.setText("DELETED AP: " + accessPoint.getId());
-        deleteLog.setType(LogType.SUCCESS);
-        logRepository.save(deleteLog);
         accessPointRepository.delete(accessPoint);
-    }
-
-    /**
-     * Creates a new access point and saves it to the database with the given input values.
-     * @param accessPoint
-     * @param location
-     * @return
-     */
-    @Transactional
-    @PreAuthorize("hasAuthority('ADMIN')")
-    public AccessPoint createAccessPoint(AccessPoint accessPoint, String location){
-
-        accessPoint.setLocation(location);
-        accessPoint.setValidated(false);
-        AccessPoint accessPointToBeCreated = saveAccessPoint(accessPoint);
-
-
-        Log createLog = new Log();
-        createLog.setDate(LocalDate.now());
-        createLog.setTime(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
-        createLog.setAuthor(getAuthenticatedUser().getUsername());
-        createLog.setSubject("AP CREATION");
-        createLog.setText("CREATED AP ID: " + accessPointToBeCreated.getId());
-        createLog.setType(LogType.SUCCESS);
-        logRepository.save(createLog);
-        return accessPointToBeCreated;
-    }
-
-    /**
-     * This Method returns the actual instance of the user.
-     * @return the authenticated user.
-     */
-    private Userx getAuthenticatedUser() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        return userRepository.findFirstByUsername(auth.getName());
     }
 }
