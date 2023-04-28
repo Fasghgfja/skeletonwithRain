22444332c244168f4c1513dec6e10cfe758f301b
Giambertone Giuliano csaz5789 <Giuliano.Giambertone@student.uibk.ac.at>
2023-04-28 12:43:15 +0200

    fileupload scope fix

--- src/main/java/at/qe/skeleton/ui/controllers/FileUploadController.java
+++ src/main/java/at/qe/skeleton/ui/controllers/FileUploadController.java
@@ -2,13 +2,17 @@ package at.qe.skeleton.ui.controllers;
 
 import at.qe.skeleton.model.Image;
 import at.qe.skeleton.services.ImageService;
+import at.qe.skeleton.ui.beans.SessionInfoBean;
 import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.ExternalContext;
 import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.file.UploadedFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -21,18 +25,37 @@ import java.io.*;
  * This class is dependent on the {@link ImageService}, which is injected using Spring's {@code @Autowired} annotation.
  */
 @Component
-@Scope("view")
+@Named
+@Scope("application")
 public class FileUploadController implements Serializable {
 
     @Autowired
     private transient ImageService imageService;
 
+    @Autowired
+    private transient SessionInfoBean sessionInfoBean;
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     /**
      * Handles the uploaded file by converting it to a byte array, saving it as an {@link at.qe.skeleton.model.Image} object using the
      * {@link ImageService}, and displaying a {@code FacesMessage} upon successful upload.
      * @param event the file upload event triggered by the user */
     public void handleFileUpload(FileUploadEvent event) throws IOException {
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        String id = externalContext.getRequestParameterMap().get("id");
+
+
+        System.out.println("Page id: " + id);
+
         FacesMessage msg = new FacesMessage("Success! ", event.getFile().getFileName() + " is uploaded.");
         FacesContext.getCurrentInstance().addMessage(null, msg);
         UploadedFile file = event.getFile();
@@ -60,7 +83,15 @@ public class FileUploadController implements Serializable {
         // Save the byte array in the Image object and persist it using imageService
         Image image = new at.qe.skeleton.model.Image();
         image.setImageByte(fileBytes);
+
+        // Save the picture to the corresponding plant
+        if(!id.equals("")){
+            imageService.addPictureToPlantPictures(image,id);
+            return ;
+        }
+
         imageService.saveImage(image);
+
     }
 
 
