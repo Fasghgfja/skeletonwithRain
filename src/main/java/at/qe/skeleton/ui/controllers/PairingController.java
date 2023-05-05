dcbf9cbfc46ef02a90714b4c7a593e2109fd0aac
Deniz Özkaya csaw1345 <deniz.oezkaya@student.uibk.ac.at>
2023-05-05 12:45:31 +0200

    pairing accesspoints to sensorstation

--- src/main/java/at/qe/skeleton/ui/controllers/PairingController.java
+++ src/main/java/at/qe/skeleton/ui/controllers/PairingController.java
@@ -96,22 +96,27 @@ public class PairingController {
 
         long startTime = System.currentTimeMillis();
         boolean sensorsFound = false;
+        interruptFlag = false;
         while (!sensorsFound && (System.currentTimeMillis() - startTime < MAX_SEARCH_TIME_MS) && !interruptFlag) {
             sensorsFound = sensorService.areSensorsPresent(sensorStationToPair );
             System.out.println(sensorService.getAllSensorsBySensorStation(sensorStationToPair)+ " " + sensorService.areSensorsPresent(sensorStationToPair));
             if (!sensorsFound) {
                 try {
                     System.out.println("im waiting 5 seconds before checking again");
-                    if(interruptFlag){break;}
+                    if(interruptFlag){
+                        interruptFlag = false;
+                        break;}
                     System.out.println("interurpt flag is" + interruptFlag);
                     // Wait for 5 seconds before checking again
-                    Thread.sleep(5000);
+                    Thread.sleep(4000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
             if (sensorsFound) {
                 System.out.println("i found sensors so im pairing");
+
+                System.out.println(interruptFlag);
                 AccessPoint accessPointToPair = accessPointService.loadAccessPoint(accessPointId);
                 sensorStationToPair.setAccessPoint(accessPointToPair);
                 sensorStationService.saveSensorStation(sensorStationToPair);
