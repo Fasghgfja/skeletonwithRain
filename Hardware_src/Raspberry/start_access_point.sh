f6167787ef15e5173d72be4b3028401abe7653e6
Stefan Huter <stefan.huter@student.uibk.ac.at>
2023-05-18 12:57:58 +0200

    do last finish steps

--- Hardware_src/Raspberry/start_access_point.sh
+++ Hardware_src/Raspberry/start_access_point.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-# shellcheck disable=SC2164
-while true
- do
-
-    # shellcheck disable=SC2164
-    echo "WARNING: Started at $(date +"%T")" &>> logFile.txt
-    python3 accesspoint_application.py;
-    echo "WARNING: Stopped at $(date +"%T")" &>> logFile.txt
-    sleep 3
-    # shellcheck disable=SC2164
-
-done
+python3 accesspoint_application.py
