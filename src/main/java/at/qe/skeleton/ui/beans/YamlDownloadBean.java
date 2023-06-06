d23d85d19fd6b74908a57078d7d633cf91129f35
Giambertone Giuliano csaz5789 <Giuliano.Giambertone@student.uibk.ac.at>
2023-06-06 14:26:32 +0200

    Yaml can be downloaded from accesspoint menagement

---
+++ src/main/java/at/qe/skeleton/ui/beans/YamlDownloadBean.java
@@ -0,0 +1,25 @@
+package at.qe.skeleton.ui.beans;
+
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+
+@Named
+@RequestScoped
+public class YamlDownloadBean {
+    private StreamedContent file;
+
+    public YamlDownloadBean() {
+        file = DefaultStreamedContent.builder()
+                .name("config.yaml")
+                .stream(() -> FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/resources/config.yaml"))
+                .build();
+    }
+
+    public StreamedContent getFile() {
+        return file;
+    }
+}
