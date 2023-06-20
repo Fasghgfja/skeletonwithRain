d358ab4043318ad9dce33c6fcb937f614fb12ff5
Tom van Loon - csaz1328 <tom.van-loon@student.uibk.ac.at>
2023-06-20 14:47:53 +0200

    created tests for CreateUserBeanTest

--- src/test/java/at/qe/skeleton/ui/beans/CreateUserBeanTest.java
+++ src/test/java/at/qe/skeleton/ui/beans/CreateUserBeanTest.java
@@ -1,7 +1,6 @@
 package at.qe.skeleton.ui.beans;
 
-import at.qe.skeleton.model.Log;
-import at.qe.skeleton.model.Userx;
+import at.qe.skeleton.model.*;
 import at.qe.skeleton.repositories.LogRepository;
 import at.qe.skeleton.repositories.UserxRepository;
 import at.qe.skeleton.services.UserService;
@@ -11,22 +10,22 @@ import jakarta.faces.context.ExternalContext;
 import jakarta.faces.context.FacesContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import static org.mockito.Mockito.*;
-
 import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class CreateUserBeanTest {
-
-    private CreateUserBean createUserBean;
+@ExtendWith(MockitoExtension.class)
+public class CreateUserBeanTest {
 
     @Mock
     private UserService userService;
@@ -46,6 +45,14 @@ class CreateUserBeanTest {
     @Mock
     private UserListController userListController;
 
+    @Mock
+    private FacesContext facesContext;
+
+    @Mock
+    private ExternalContext externalContext;
+
+    private CreateUserBean createUserBean;
+
     @BeforeEach
     void setUp() {
         createUserBean = new CreateUserBean();
@@ -55,87 +62,42 @@ class CreateUserBeanTest {
         createUserBean.setLogRepository(logRepository);
         createUserBean.setSessionInfoBean(sessionInfoBean);
         createUserBean.setUserListController(userListController);
+        createUserBean.setSelectedRoles(Arrays.asList("ADMIN", "GARDENER"));
+        createUserBean.setUsername("testuser");
+        createUserBean.setPassword("testpassword");
+        createUserBean.setEmail("test@example.com");
+        createUserBean.setFirstName("Test");
+        createUserBean.setLastName("User");
+        createUserBean.setPhone("1234567890");
+
+        when(facesContext.getExternalContext()).thenReturn(externalContext);
     }
 
 
-
     @Test
-    void registerNewUser_UsernameTaken() throws IOException {
-        // Mock dependencies
-        UserxRepository userxRepository = mock(UserxRepository.class);
-        PasswordEncoder passwordEncoder = mock(PasswordEncoder.class);
-        SessionInfoBean sessionInfoBean = mock(SessionInfoBean.class);
-        LogRepository logRepository = mock(LogRepository.class);
-        UserService userService = mock(UserService.class); // Create a mock UserService
-
-        when(userxRepository.findFirstByUsername(anyString())).thenReturn(new Userx());
-
-        // Create the instance of the class under test and set the dependencies
-        CreateUserBean createUserBean = new CreateUserBean();
-        createUserBean.setUserxRepository(userxRepository);
-        createUserBean.setPasswordEncoder(passwordEncoder);
-        createUserBean.setSessionInfoBean(sessionInfoBean);
-        createUserBean.setLogRepository(logRepository);
-        createUserBean.setUserService(userService); // Set the UserService
-
-        // Mock FacesContext
-        try (MockedStatic<FacesContext> mockedFacesContext = mockStatic(FacesContext.class)) {
-            FacesContext facesContext = mock(FacesContext.class);
-            ExternalContext externalContext = mock(ExternalContext.class);
-            when(facesContext.getExternalContext()).thenReturn(externalContext);
-            mockedFacesContext.when(FacesContext::getCurrentInstance).thenReturn(facesContext);
-
-            // Call the method
-            createUserBean.registerNewUser();
-
-            // Verify that the appropriate actions were performed
-            // For example, verify that a warning log was created
-            verify(logRepository).save(any(Log.class));
-            // Verify that the user is redirected to the login page
-            verify(externalContext).redirect(contains("/login.xhtml"));
-        }
-    }
-
-
-
-
-    @Test
-    void registerNewUser_Success() throws IOException {
-        // Mock dependencies
-        UserxRepository userxRepository = mock(UserxRepository.class);
-        PasswordEncoder passwordEncoder = mock(PasswordEncoder.class);
-        SessionInfoBean sessionInfoBean = mock(SessionInfoBean.class);
-        LogRepository logRepository = mock(LogRepository.class);
-        UserService userService = mock(UserService.class);
-        ExternalContext externalContext = mock(ExternalContext.class);
-
-        when(userxRepository.findFirstByUsername(anyString())).thenReturn(null);
-
-        // Create the instance of the class under test and set the dependencies
-        CreateUserBean createUserBean = new CreateUserBean();
-        createUserBean.setUserxRepository(userxRepository);
-        createUserBean.setPasswordEncoder(passwordEncoder);
-        createUserBean.setSessionInfoBean(sessionInfoBean);
-        createUserBean.setLogRepository(logRepository);
-        createUserBean.setUserService(userService);
-
-        // Mock FacesContext and ExternalContext
-        try (MockedStatic<FacesContext> mocked = mockStatic(FacesContext.class)) {
-            FacesContext facesContext = mock(FacesContext.class);
-            when(facesContext.getExternalContext()).thenReturn(externalContext);
+    void testRegisterNewUser() throws Exception {
+        try (MockedStatic<FacesContext> mocked = Mockito.mockStatic(FacesContext.class)) {
             mocked.when(FacesContext::getCurrentInstance).thenReturn(facesContext);
+            // when username is already taken
+            when(userxRepository.findFirstByUsername(anyString())).thenReturn(new Userx());
 
-            // Call the method
             createUserBean.registerNewUser();
-
-            // Verify that the appropriate actions were performed
-            // For example, verify that a success log was created
             verify(logRepository).save(any(Log.class));
-            // Verify that the user was saved
+            verify(userService, never()).saveUser(any(Userx.class));
+            verify(externalContext).redirect(anyString());
+
+            // Resetting interactions for the next scenario
+            reset(userxRepository, userService, logRepository, externalContext);
+
+            // when username is not taken
+            when(userxRepository.findFirstByUsername(anyString())).thenReturn(null);
+            when(passwordEncoder.encode(anyString())).thenReturn("encodedpassword");
+            when(sessionInfoBean.getCurrentUserName()).thenReturn("admin");
+
+            createUserBean.registerNewUser();
             verify(userService).saveUser(any(Userx.class));
-            // Verify that the user is redirected to the login page
-            verify(externalContext).redirect(contains("/login.xhtml"));
+            verify(logRepository).save(any(Log.class)); // count increased due to successful registration
+            verify(externalContext).redirect(anyString());
         }
     }
-
 }
