6fbf34435b84e77c5039f523ef09079efd06b0e1
Giambertone Giuliano csaz5789 <Giuliano.Giambertone@student.uibk.ac.at>
2023-11-16 20:20:15 +0100

    update

--- src/main/java/at/qe/skeleton/configs/WebSecurityConfig.java
+++ src/main/java/at/qe/skeleton/configs/WebSecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -56,6 +55,7 @@ public class WebSecurityConfig {
                             .permitAll()
                             .defaultSuccessUrl("/dashboard.xhtml")
                             .loginProcessingUrl("/login")
+                            .successForwardUrl("/dashboard.xhtml")
                             .failureUrl("/login.xhtml?error=true")
                     )
                     .logout(logout -> logout
